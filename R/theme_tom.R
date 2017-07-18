654862c355aa5414392c52529c84f031fa45f262
Tom McMahon <thmcmahon@gmail.com>
2017-07-18 11:11:04 +1000

    First commit

---
+++ R/theme_tom.R
@@ -0,0 +1,22 @@
+#' Tom's sweet theme.
+#'
+#' @param base_size base font size
+#' @param base_family base font family
+#'
+#' @export
+#'
+#' @import ggplot2
+theme_tom <- function (base_size = 11, base_family = "") {
+  half_line <- base_size/2
+  theme_bw(
+    base_size = base_size, base_family = base_family
+  ) %+replace%
+    theme(
+      panel.border = element_rect(fill = NA, colour = "black", size = .2),
+      panel.grid.minor = element_blank(),
+      panel.grid.major = element_line(colour = "black", size = .05),
+      plot.caption = element_text(size = rel(0.9), hjust = 0,
+                                  vjust = 1,
+                                  margin = margin(t = half_line * 0.9))
+    )
+}
